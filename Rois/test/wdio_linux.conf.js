2e19336c20f7c031d07d27105cd2c5620347b41a
Hiroka Hamada <hamada.hiroka@gmail.com>
2020-02-05 15:26:05 +0900

    change browser. firefox -> headless chrome.

--- Rois/test/wdio_linux.conf.js
+++ Rois/test/wdio_linux.conf.js
@@ -51,11 +51,14 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 3,
         //
-        browserName: 'firefox',
+        browserName: 'chrome',
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
+        chromeOptions: {
+            args: ['headless', 'disable-gpu']
+        },
     },
     //{
     //    maxInstances: 1,
