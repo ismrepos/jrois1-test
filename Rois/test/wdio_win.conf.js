41e8ca4dddeb21cd88795ef3a47f4f5e371bf3e1
Hamada Hiroka <hamada.hiroka@miraitsystems.jp>
2020-03-03 15:04:56 +0900

    delete headless option.

--- Rois/test/wdio_win.conf.js
+++ Rois/test/wdio_win.conf.js
@@ -53,7 +53,7 @@ exports.config = {
         //
         //browserName: 'internet explorer',
         browserName: 'firefox',
-        'moz:firefoxOptions': { args: ['-headless']},
+        //'moz:firefoxOptions': { args: ['-headless']},
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
