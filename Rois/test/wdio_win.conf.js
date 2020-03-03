81c0c0de2aad067a28f9790b73ad32ac8934a3ac
Hamada Hiroka <hamada.hiroka@miraitsystems.jp>
2020-03-03 14:54:14 +0900

    test.

--- Rois/test/wdio_win.conf.js
+++ Rois/test/wdio_win.conf.js
@@ -51,7 +51,9 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 3,
         //
-        browserName: 'internet explorer',
+        //browserName: 'internet explorer',
+        browserName: 'firefox',
+        'moz:firefoxOptions': { args: ['-headless']},
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
